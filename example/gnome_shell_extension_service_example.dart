30f597fa96262f1bf19a1ced7014169c325b9333
Feichtmeier <frederik.feichtmeier@gmail.com>
2023-01-04 16:10:09 +0100

    Update export and add signal

--- example/gnome_shell_extension_service_example.dart
+++ example/gnome_shell_extension_service_example.dart
@@ -1,6 +1,1 @@
-import 'package:gnome_shell_extension_service/gnome_shell_extension_service.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+void main() {}
--- test/gnome_shell_extension_service_test.dart
+++ test/gnome_shell_extension_service_test.dart
@@ -1,16 +1,13 @@
-import 'package:gnome_shell_extension_service/gnome_shell_extension_service.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
     setUp(() {
       // Additional setup goes here.
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      expect(true, isTrue);
     });
   });
 }
